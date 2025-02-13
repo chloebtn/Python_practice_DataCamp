e063aca75ddec00d54a3b223299a35349a46691b
Chloé Bouton <bouton.chloe.98@gmail.com>
2025-02-13 20:14:45 +0000

    Add code

--- Stock-Analysis/Portfolio_Risks_and_Returns.py
+++ Stock-Analysis/Portfolio_Risks_and_Returns.py
@@ -1,1 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
+# Load Data
+stock_prices_df = pd.read_csv('/workspaces/Python/Stock-Analysis/faang_stocks.csv', index_col='Date')
+
+# Changing the index to a datetime type allowing easier filtering and plotting
+stock_prices_df.index = pd.to_datetime(stock_prices_df.index)
+
+print(stock_prices_df)
+
+# Plotting the stock prices
+stock_prices_df.plot(title='FAANG stock prices from years 2020-2023')
+plt.show()
