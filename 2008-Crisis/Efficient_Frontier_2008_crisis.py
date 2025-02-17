4c646f0acbf0433f08e82dedf73e7e96629f8f77
Chloé Bouton <bouton.chloe.98@gmail.com>
2025-02-17 20:38:47 +0000

    Update

--- 2008-Crisis/Efficient_Frontier_2008_crisis.py
+++ 2008-Crisis/Efficient_Frontier_2008_crisis.py
@@ -46,6 +46,15 @@ print('Efficient Covariance Matrices\n', e_cov)
     # The matrices show how the portfolio's risk increased during the crisis
 
 
+# returns and covariance matrix before alone
+prices_before = prices.loc[epochs['before']['start']:epochs['before']['end']]
+prices_before = prices_before.drop(columns=['Citibank'], errors='ignore')
+returns_before = prices_before.pct_change().dropna()
+returns_before = returns_before.mean()
+
+ecov_before = CovarianceShrinkage(prices_before).ledoit_wolf()
+
+
 # returns and covariance matrix during alone
 prices_during = prices.loc[epochs['during']['start']:epochs['during']['end']]
 prices_during = prices_during.drop(columns=['Citibank'], errors='ignore')
@@ -55,16 +64,62 @@ returns_during = returns_during.mean()
 ecov_during = CovarianceShrinkage(prices_during).ledoit_wolf()
 
 
+# returns and covariance matrix after alone
+prices_after = prices.loc[epochs['after']['start']:epochs['after']['end']]
+prices_after = prices_after.drop(columns=['Citibank'], errors='ignore')
+returns_after = prices_after.pct_change().dropna()
+returns_after = returns_after.mean()
+
+ecov_after = CovarianceShrinkage(prices_after).ledoit_wolf()
+
+
 # Critical Line Algorithm
+efficient_portfolio_before = CLA(returns_before, ecov_before)
+print(efficient_portfolio_before.min_volatility())
+
 efficient_portfolio_during = CLA(returns_during, ecov_during)
 print(efficient_portfolio_during.min_volatility())
 
-# Efficient frontier
-(ret, vol, weights) = efficient_portfolio_during.efficient_frontier()
+efficient_portfolio_after = CLA(returns_after, ecov_after)
+print(efficient_portfolio_after.min_volatility())
 
-plt.scatter(vol, ret, s=4, c='g', marker='.')
+
+# Efficient frontiers before during after together
+(ret_b, vol_b, weights_b) = efficient_portfolio_before.efficient_frontier()
+(ret_d, vol_d, weights_d) = efficient_portfolio_during.efficient_frontier()
+(ret_a, vol_a, weights_a) = efficient_portfolio_after.efficient_frontier()
+plt.scatter(vol_b, ret_b, s=4, c='g', marker='.', label='Before')
+plt.scatter(vol_d, ret_d, s=4, c='r', marker='.', label='During')
+plt.scatter(vol_a, ret_a, s=4, c='b', marker='.', label='After')
+plt.xlabel('Standard Deviation')
+plt.ylabel('Expected Return')
+plt.title('Efficient Frontier Before/During/After the Crisis')
+plt.legend()
+plt.show()
+
+# Efficient frontiers separated
+(ret_b, vol_b, weights_b) = efficient_portfolio_before.efficient_frontier()
+plt.scatter(vol_b, ret_b, s=4, c='g', marker='.', label='Before')
+plt.xlabel('Standard Deviation')
+plt.ylabel('Expected Return')
+plt.title('Efficient Frontier Before the Crisis')
+plt.legend()
+plt.show()
+
+
+(ret_d, vol_d, weights_d) = efficient_portfolio_during.efficient_frontier()
+plt.scatter(vol_d, ret_d, s=4, c='r', marker='.', label='During')
 plt.xlabel('Standard Deviation')
 plt.ylabel('Expected Return')
 plt.title('Efficient Frontier During the Crisis')
+plt.legend()
+plt.show()
+
+
+(ret_a, vol_a, weights_a) = efficient_portfolio_after.efficient_frontier()
+plt.scatter(vol_a, ret_a, s=4, c='b', marker='.', label='After')
+plt.xlabel('Standard Deviation')
+plt.ylabel('Expected Return')
+plt.title('Efficient Frontier After the Crisis')
 plt.legend()
 plt.show()
